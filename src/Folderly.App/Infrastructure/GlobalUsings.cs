b81a67df84b1277aa22724a1b90fafad8f09bf5f
s-knk <625.somq2525@gmail.com>
2026-05-20 13:58:13 +0900

    fix: avoid Path ambiguity in WPF build

--- src/Folderly.App/Infrastructure/GlobalUsings.cs
+++ src/Folderly.App/Infrastructure/GlobalUsings.cs
@@ -1,2 +1,1 @@
 global using System.IO;
-global using System.Windows.Shapes;
--- src/Folderly.App/Views/ApplyWindow.xaml.cs
+++ src/Folderly.App/Views/ApplyWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Folderly.App.Views;
 
